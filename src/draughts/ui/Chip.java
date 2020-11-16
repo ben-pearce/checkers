1d23e679bb07ee8cd3c7549dd2a6fee1685c7b04
Ben <ben.pearce@sky.com>
2020-11-16 16:56:08 +0000

    Cell and Chip UI classes

--- src/draughts/ui/Chip.java
+++ src/draughts/ui/Chip.java
@@ -10,20 +10,32 @@ import javafx.scene.shape.Circle;
 
 public class Chip extends Circle {
 
-    private final EventHandler<MouseEvent> pressed;
+    /**
+     * Is chip in an interactive state.
+     */
+    private boolean interactive = false;
+
+    /**
+     * Is chip in a lifted state.
+     */
+    private boolean lifted = false;
+
+    /**
+     * Event handler when mouse enters chip.
+     */
     private final EventHandler<MouseEvent> entered;
+
+    /**
+     * Event handler when mouse exits chip.
+     */
     private final EventHandler<MouseEvent> exited;
 
 
+    /**
+     * Chip UI element for checkers game.
+     */
     public Chip(double v, double v1, double v2, Paint paint) {
         super(v, v1, v2, paint);
-        pressed = mouseEvent -> {
-            DropShadow ds = new DropShadow();
-            ds.setOffsetX(0);
-            ds.setOffsetY(0);
-            this.setEffect(ds);
-            this.setStroke(null);
-        };
 
         entered = mouseEvent -> {
             this.setStroke(Color.BLACK);
@@ -33,21 +45,55 @@ public class Chip extends Circle {
         exited = mouseEvent -> {
             this.setStroke(null);
         };
-
     }
 
-    public void setActive(boolean active) {
-        if(active) {
+    /**
+     * @return Is chip in interactive state.
+     */
+    public boolean isInteractive() {
+        return interactive;
+    }
+
+    /**
+     * @return Is chip in lifted state.
+     */
+    public boolean isLifted() {
+        return lifted;
+    }
+
+    /**
+     * Set chip lifted state.
+     *
+     * @param lifted    Chip lifted state.
+     */
+    public void setLifted(boolean lifted) {
+        if(lifted) {
+            DropShadow ds = new DropShadow();
+            ds.setOffsetX(0);
+            ds.setOffsetY(0);
+            this.setEffect(ds);
+            this.setStroke(null);
+        } else {
+            this.setEffect(null);
+        }
+        this.lifted = lifted;
+    }
+
+    /**
+     * Set chip interactive state.
+     *
+     * @param interactive    Chip interactive state.
+     */
+    public void setInteractive(boolean interactive) {
+        if(interactive) {
             this.setCursor(Cursor.HAND);
-            this.addEventHandler(MouseEvent.MOUSE_PRESSED, pressed);
             this.addEventHandler(MouseEvent.MOUSE_ENTERED, entered);
             this.addEventHandler(MouseEvent.MOUSE_EXITED, exited);
         } else {
             this.setCursor(Cursor.DEFAULT);
-            this.setEffect(null);
-            this.removeEventHandler(MouseEvent.MOUSE_PRESSED, pressed);
             this.removeEventHandler(MouseEvent.MOUSE_ENTERED, entered);
             this.removeEventHandler(MouseEvent.MOUSE_EXITED, exited);
         }
+        this.interactive = interactive;
     }
 }
