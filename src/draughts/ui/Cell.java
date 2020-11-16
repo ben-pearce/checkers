4d6152ca11bcc54c03dbce89248d481811a2b046
Ben <ben@solero.me>
2020-11-16 16:56:08 +0000

    Cell and Chip UI classes

--- src/draughts/ui/Cell.java
+++ src/draughts/ui/Cell.java
@@ -3,7 +3,6 @@ package draughts.ui;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Cursor;
-import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -13,30 +12,78 @@ import javafx.scene.paint.Color;
 
 public class Cell extends Pane {
 
+    /**
+     * Is cell in an interactive state.
+     */
+    private boolean interactive = false;
+
+    /**
+     * Event handler when mouse enters cell.
+     */
+    private final EventHandler<MouseEvent> entered;
+
+    /**
+     * Event handler when mouse exits cell.
+     */
+    private final EventHandler<MouseEvent> exited;
+
+    /**
+     * Cell UI element for checkers game.
+     */
     public Cell() {
         super();
-    }
 
-    public void setActive(boolean active) {
-        if(active) {
-            EventHandler<MouseEvent> selected = mouseEvent -> {
-                this.setActive(false);
-            };
+        entered = mouseEvent -> {
+            this.setBackground(new Background(new BackgroundFill(
+                    Color.web("#1db345"),
+                    CornerRadii.EMPTY,
+                    Insets.EMPTY
+            )));
+        };
 
-            this.setCursor(Cursor.HAND);
+        exited = mouseEvent -> {
             this.setBackground(new Background(new BackgroundFill(
                     Color.web("#1fd14f"),
                     CornerRadii.EMPTY,
                     Insets.EMPTY
             )));
-            this.addEventHandler(MouseEvent.MOUSE_PRESSED, selected);
+        };
+    }
+
+    /**
+     * @return Is cell in interactive state.
+     */
+    public boolean isInteractive() {
+        return interactive;
+    }
+
+    /**
+     * Set cell interactive state.
+     *
+     * @param interactive   Cell interactive state.
+     */
+    public void setInteractive(boolean interactive) {
+        if(interactive) {
+            this.setBackground(new Background(new BackgroundFill(
+                    Color.web("#1fd14f"),
+                    CornerRadii.EMPTY,
+                    Insets.EMPTY
+            )));
+
+            this.setCursor(Cursor.HAND);
+            this.addEventHandler(MouseEvent.MOUSE_ENTERED, entered);
+            this.addEventHandler(MouseEvent.MOUSE_EXITED, exited);
         } else {
-            this.setCursor(Cursor.DEFAULT);
             this.setBackground(new Background(new BackgroundFill(
                     Color.web("#a67d5d"),
                     CornerRadii.EMPTY,
                     Insets.EMPTY
             )));
+
+            this.setCursor(Cursor.DEFAULT);
+            this.removeEventHandler(MouseEvent.MOUSE_ENTERED, entered);
+            this.removeEventHandler(MouseEvent.MOUSE_EXITED, exited);
         }
+        this.interactive = interactive;
     }
 }
