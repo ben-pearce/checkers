a0bb5a2b694d272229b33fed18e62d1785822f0d
Ben <ben@solero.me>
2020-11-17 23:37:29 +0000

    Chip classes

---
+++ src/checkers/Chip.java
@@ -0,0 +1,14 @@
+package checkers;
+
+public class Chip {
+
+    private boolean king = false;
+
+    public boolean isKing() {
+        return king;
+    }
+
+    public void setKing(boolean king) {
+        this.king = king;
+    }
+}
