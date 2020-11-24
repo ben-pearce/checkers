d43298e28dc395e338642ff04827047d9855d769
Ben <ben.pearce@sky.com>
2020-11-24 00:56:10 +0000

    Reduce chip parent classes to an attribute

--- src/checkers/Chip.java
+++ src/checkers/Chip.java
@@ -4,19 +4,26 @@ public class Chip {
 
     private boolean king = false;
 
+    private final int player;
+
+    public Chip(Chip c) {
+        king = c.king;
+        player = c.player;
+    }
+
+    public Chip(int player) {
+        this.player = player;
+    }
+
+    public int getPlayer() {
+        return player;
+    }
+
     public boolean isKing() {
         return king;
     }
 
     public void setKing(boolean king) {
         this.king = king;
-    }
-
-    public Chip(Chip c) {
-        king = c.king;
-    }
-
-    public Chip() {
-
     }
 }
