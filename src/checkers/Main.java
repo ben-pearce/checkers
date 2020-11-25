f1f0495eb1d5f44d063df01996d096445dcb6728
Ben <ben@solero.me>
2020-11-25 21:00:06 +0000

    Invoke game menu on start

--- src/checkers/Main.java
+++ src/checkers/Main.java
@@ -1,6 +1,7 @@
 package checkers;
 
 import checkers.ui.Game;
+import checkers.ui.Menu;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -9,12 +10,29 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Game g = new Game();
-        Scene scene = new Scene(g);
-        primaryStage.setTitle("Checkers");
-        primaryStage.setScene(scene);
+        Menu m = new Menu();
+        Scene menuScene = new Scene(m);
+
+        primaryStage.setTitle("Checkers Setup");
+        primaryStage.setScene(menuScene);
         primaryStage.setResizable(false);
         primaryStage.show();
+
+        m.setOnGameOpen(e -> {
+            primaryStage.hide();
+            Game g = new Game(m.getGameConfig());
+            g.setOnGameExit(e1 -> {
+                primaryStage.hide();
+                primaryStage.setTitle("Checkers Setup");
+                primaryStage.setScene(menuScene);
+                primaryStage.show();
+            });
+
+            Scene gameScene = new Scene(g);
+            primaryStage.setTitle("Checkers");
+            primaryStage.setScene(gameScene);
+            primaryStage.show();
+        });
     }
 
     public static void main(String[] args) {
